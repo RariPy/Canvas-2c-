03c47b704a214272e93d1a3d1df78ec38a65435f
Sami <sa17022025@gmail.com>
2025-03-11 14:36:11 +0100

    First Draft

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,19 +6,65 @@ using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
-    public static void Main(string[] args)
-    
-    {
+    public static void Main(string[] args)   
+    {  
         Dictionary<string, int> shop = new Dictionary<string, int >();
-        shop.Add("a", 90);
-        shop.Add("b", 34);
-        shop.Add("c", 85);
-        shop.Add("d", 80);
-        shop.Add("e", 11);
-        shop.Add("f", 26);                
-        foreach (var ele in shop)
+        shop.Add("a", 10);
+        shop.Add("b", 20);
+        shop.Add("c", 30);
+        shop.Add("d", 50);
+        shop.Add("e", 70);
+        shop.Add("f", 80);
+
+        foreach (var item in shop)
         {
-            Console.WriteLine($"Key: {ele.Key}, Value: {ele.Value}");
+           int Discount = GetDiscount(item.Value); 
+            Console.WriteLine($"item {item.Key}, quantity:{item.Value}, Discount = {Discount}%" );
+        }
+        while (true)
+        {
+           Console.WriteLine("Enter item name"); 
+           string ItemName = Console.ReadLine();
+           Console.WriteLine($"Enter quantity for {ItemName}");
+           int quantity = Convert.ToInt32(Console.ReadLine());
+           shop.Add(ItemName, quantity);
+           break;
+        }
+        foreach (var item in shop)
+        {
+            int Discount = GetDiscount(item.Value); 
+             Console.WriteLine($"item {item.Key}, quantity:{item.Value}, Discount = {Discount}%" );
+        }
+   
+    }
+    // (Publtic static int) creates a public class that allows (int Discount = GetDiscount(item.Value);) to access it 
+    public static int GetDiscount (int quantity) 
+    {
+        if (quantity >= 0 && quantity <= 19)
+        {
+            return 0;
+        }
+        else if (quantity >= 20 && quantity <= 39)
+        {
+            return 10;
+        }
+        else if (quantity >= 40 && quantity <= 59)
+        {
+            return 20;
+        }
+       else if (quantity >= 60 && quantity <= 79)
+        {
+            return 30;
+        }
+        else if (quantity >= 80 && quantity <= 10)
+        {
+            return 40;
+        }
+        else 
+        {
+            return 40;
         }
     }
+
 }
+
