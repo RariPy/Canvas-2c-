86bcb2f42644addcaa5cf09d26b8e816bc3be20d
Sami <sa17022025@gmail.com>
2025-03-18 13:12:34 +0100

    Final.v2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,6 +1,4 @@
 ﻿namespace task2;
-using System;
-
 class Program
 {
     public static void Main(string[] args)   
@@ -14,29 +12,35 @@ class Program
             { "e", 70 },
             { "f", 80 }
         };
-        //Shows the base dictionary
-        foreach (var item in shop)
+        string restart = "yes";
+        while (restart == "yes")
         {
-           int Discount = GetDiscount(item.Value); 
-            Console.WriteLine($"item {item.Key}, quantity:{item.Value}, Discount = {Discount}%" );
-        }
-        // prompts the user to input an item
-        while (true)
-        {
-           Console.WriteLine("Enter item name"); 
-           string ItemName = Console.ReadLine();
-           Console.WriteLine($"Enter quantity for {ItemName}");
-           int quantity = Convert.ToInt32(Console.ReadLine());
-           shop.Add(ItemName, quantity);
-           break;
-        }
-        //shows the updated dictionary after user has inputted a new item
-        foreach (var item in shop) 
-        {
+            //Shows the base dictionary
+            foreach (var item in shop)
+            {
             int Discount = GetDiscount(item.Value); 
-             Console.WriteLine($"item {item.Key}, quantity:{item.Value}, Discount = {Discount}%" );
+                Console.WriteLine($"item {item.Key}, quantity:{item.Value}, Discount = {Discount}%" );
+            }
+            // prompts the user to input an item
+            while (true)
+            {
+            Console.WriteLine("Enter item name"); 
+            string ItemName = Console.ReadLine();
+            Console.WriteLine($"Enter quantity for {ItemName}");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            shop.Add(ItemName, quantity);
+            break;
+            }
+            //shows the updated dictionary after user has inputted a new item
+            foreach (var item in shop) 
+            {
+                int Discount = GetDiscount(item.Value); 
+                Console.WriteLine($"item {item.Key}, quantity:{item.Value}, Discount = {Discount}%" );
+            }
+
+            Console.WriteLine("Do you wish to enter more items to the list Yes/No");
+            restart = Console.ReadLine().ToLower();
         }
-   
     }
     // Assigns discount depending on item quantity, with a max value of 40% on anything higher than 80.
     public static int GetDiscount (int quantity) 
